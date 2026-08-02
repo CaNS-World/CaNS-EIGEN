09001c99f7e007c81ecf77880559b704f1f96b70
Pedro Costa <p.simoes.costa@gmail.com>
2026-08-02 12:08:33 +0200

    Avoid redundant pressure-correction halo work
    Adapt CaNS commit b9b512f to EIGEN's nonuniform metric arrays and solver interfaces.

--- src/main.f90
+++ src/main.f90
@@ -480,8 +480,9 @@ program cans
     if(myid == 0) print*, '*** Checkpoint loaded at time = ', time, 'time step = ', istep, '. ***'
   end if
   !$acc enter data copyin(u,v,w,p,dudtrko,dvdtrko,dwdtrko) create(pp)
-  call bounduvw(cbcvel,n,bcvel,nb,is_bound,.false.,dxc,dxf,dyc,dyf,dzc,dzf,u,v,w)
-  call boundp(cbcpre,n,bcpre,nb,is_bound,dxc,dyc,dzc,p)
+  call bounduvw(cbcvel,n,bcvel,nb,is_bound,dxc,dxf,dyc,dyf,dzc,dzf,u,v,w)
+  ! Momentum uses forward pressure differences, so only upper `p` halos are read.
+  call boundp(cbcpre,n,bcpre,nb,is_bound,dxc,dyc,dzc,p,1)
   do iscal=1,nscal
     s => scalars(iscal)
     !$acc enter data copyin(s%val,s%dsdtrko) async(1)
@@ -559,7 +560,8 @@ program cans
                              lambdayw_g,lambdaxyw,eigvecxw_fwd,eigvecxw_bwd,eigvecyw_fwd,eigvecyw_bwd, &
                              aw,bw,cw,rhsbw%x,rhsbw%y,rhsbw%z,is_bound,cbcvel(:,:,3),['c','c','f'],w)
       end if
-      call bounduvw(cbcvel,n,bcvel,nb,is_bound,.false.,dxc,dxf,dyc,dyf,dzc,dzf,u,v,w)
+      ! `fillps` needs lower normal halos; wide `correc` also needs both tangential halos.
+      call bounduvw(cbcvel,n,bcvel,nb,is_bound,dxc,dxf,dyc,dyf,dzc,dzf,u,v,w,.false.,-1)
       call fillps(n,dxfi,dyfi,dzfi,dtrki,u,v,w,pp)
       call updt_rhs_b(['c','c','c'],cbcpre,n,is_bound,rhsbp%x,rhsbp%y,rhsbp%z,pp)
       call solver(n,ng,is_poisson_fft,arrplanp,product(normfftp(:)), &
@@ -567,9 +569,10 @@ program cans
                   ap,bp,cp,cbcpre,['c','c','c'],pp,is_dtdma_update_p,ap_d,cp_d)
       call boundp(cbcpre,n,bcpre,nb,is_bound,dxc,dyc,dzc,pp)
       call correc(n,dxci,dyci,dzci,dtrk,pp,u,v,w)
-      call bounduvw(cbcvel,n,bcvel,nb,is_bound,.true.,dxc,dxf,dyc,dyf,dzc,dzf,u,v,w)
+      ! `correc` updates the tangential and lower normal halos locally.
+      call bounduvw(cbcvel,n,bcvel,nb,is_bound,dxc,dxf,dyc,dyf,dzc,dzf,u,v,w,.true.,1,0)
       call updatep(n,dxci,dxfi,dyci,dyfi,dzci,dzfi,alpha,pp,p)
-      call boundp(cbcpre,n,bcpre,nb,is_bound,dxc,dyc,dzc,p)
+      call boundp(cbcpre,n,bcpre,nb,is_bound,dxc,dyc,dzc,p,1)
     end do
     dpdl(:)     = -dpdl(:)*dti
     fs(1:nscal) = fs(1:nscal)*dti
